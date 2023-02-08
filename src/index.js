58f2b8fa68b3d803223f73d8299561c3137b2388
Andrew Kane <andrew@ankane.org>
2023-02-08 12:28:04 -0800

    Updated error message

--- src/index.js
+++ src/index.js
@@ -210,11 +210,11 @@ class BaseMap {
           const coordinates = rowCoordinates(row)
 
           if (!coordinates[1]) {
-            throw new Error(`missing latitude for index ${i}`)
+            throw new Error(`missing latitude (index: ${i})`)
           }
 
           if (!coordinates[0]) {
-            throw new Error(`missing longitude for index ${i}`)
+            throw new Error(`missing longitude (index: ${i})`)
           }
 
           geometry = {
@@ -225,7 +225,7 @@ class BaseMap {
           geometry = row.geometry
 
           if (!geometry) {
-            throw new Error(`missing geometry for index ${i}`)
+            throw new Error(`missing geometry (index: ${i})`)
           }
 
           delete properties.geometry
